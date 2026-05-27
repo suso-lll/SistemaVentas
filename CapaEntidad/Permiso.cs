28a1eea68521670cbc0bd8e369e1ffbce85228b8
suso-lll <susomar.00@gmail.com>
2026-05-27 12:16:55 -0700

    first commit

---
+++ CapaEntidad/Permiso.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Permiso
+    {
+        public string IdPermiso { get; set; }
+        public Rol odRol { get; set; }
+        public string NombreMenu { get; set; }
+        public string FechaRegistro { get; set; }
+    }
+}
